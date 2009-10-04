0b15b751158ba25a2025c31b418a139c70bf92a2
John Mair <jrmair@gmail.com>
2009-10-04 12:23:44 -0400

    hopefully Rakefile can build universal macosx binaries

--- ext/chipmunk/extconf.rb
+++ ext/chipmunk/extconf.rb
@@ -1,4 +1,11 @@
 require 'mkmf'
 
+if ARGV[0]  == "macosx"
+    print "display ARGV"
+    puts ARGV.inspect
+    $CFLAGS += ' -arch ppc -arch i386'
+    $LDFLAGS += ' -arch i386 -arch ppc'
+end
+
 $CFLAGS += ' -std=gnu99 -ffast-math'
 create_makefile('chipmunk')
