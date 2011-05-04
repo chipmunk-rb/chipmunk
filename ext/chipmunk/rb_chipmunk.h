54ac2f34aab80dc93738c224a4cba5fff649fe85
Beoran <beoran@rubyforge.org>
2011-05-04 22:59:33 +0200

    Uncrustfied C code using uncrustify.cnf .

--- ext/chipmunk/rb_chipmunk.h
+++ ext/chipmunk/rb_chipmunk.h
@@ -1,15 +1,15 @@
 /* Copyright (c) 2007 Scott Lembcke
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -18,7 +18,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
- 
+
 extern VALUE m_Chipmunk;
 
 extern VALUE c_cpVect;
@@ -37,39 +37,37 @@ extern VALUE c_cpSegmentQueryInfo;
 extern ID id_parent;
 
 static inline VALUE
-VNEW(cpVect v)
-{
-	cpVect *ptr = malloc(sizeof(cpVect));
-	*ptr = v;
-	return Data_Wrap_Struct(c_cpVect, NULL, free, ptr);	
+VNEW(cpVect v) {
+  cpVect *ptr = malloc(sizeof(cpVect));
+  *ptr = v;
+  return Data_Wrap_Struct(c_cpVect, NULL, free, ptr);
 }
 
 static inline VALUE
-VWRAP(VALUE parent, cpVect *v)
-{
-	VALUE vec_obj = Data_Wrap_Struct(c_cpVect, NULL, NULL, v);
-	rb_ivar_set(vec_obj, id_parent, parent);
-	
-	return vec_obj;	
+VWRAP(VALUE parent, cpVect *v) {
+  VALUE vec_obj = Data_Wrap_Struct(c_cpVect, NULL, NULL, v);
+  rb_ivar_set(vec_obj, id_parent, parent);
+
+  return vec_obj;
 }
 
-#define GETTER_TEMPLATE(func_name, klass, type)\
-static inline type *\
-func_name(VALUE self)\
-{\
-	if(!rb_obj_is_kind_of(self, klass)){\
-		VALUE klass_name = rb_funcall(klass, rb_intern("to_s"), 0);\
-		rb_raise(rb_eTypeError, "wrong argument type %s (expected %s)", rb_obj_classname(self), StringValuePtr(klass_name));\
-	}\
-	type *ptr = NULL;\
-	Data_Get_Struct(self, type, ptr);\
-	return ptr;\
-}\
+#define GETTER_TEMPLATE(func_name, klass, type)                                                                            \
+  static inline type *                                                                                                     \
+  func_name(VALUE self)                                                                                                    \
+  {                                                                                                                        \
+    if(!rb_obj_is_kind_of(self, klass)) {                                                                                  \
+      VALUE klass_name = rb_funcall(klass, rb_intern("to_s"), 0);                                                          \
+      rb_raise(rb_eTypeError, "wrong argument type %s (expected %s)", rb_obj_classname(self), StringValuePtr(klass_name)); \
+    }                                                                                                                      \
+    type *ptr = NULL;                                                                                                      \
+    Data_Get_Struct(self, type, ptr);                                                                                      \
+    return ptr;                                                                                                            \
+  }                                                                                                                        \
 
-GETTER_TEMPLATE(VGET , c_cpVect , cpVect )
-GETTER_TEMPLATE(BBGET, c_cpBB   , cpBB   )
-GETTER_TEMPLATE(BODY , c_cpBody , cpBody )
-GETTER_TEMPLATE(STATICBODY , c_cpStaticBody , cpBody)
+GETTER_TEMPLATE(VGET, c_cpVect, cpVect )
+GETTER_TEMPLATE(BBGET, c_cpBB, cpBB   )
+GETTER_TEMPLATE(BODY, c_cpBody, cpBody )
+GETTER_TEMPLATE(STATICBODY, c_cpStaticBody, cpBody)
 GETTER_TEMPLATE(SHAPE, m_cpShape, cpShape)
 GETTER_TEMPLATE(CONSTRAINT, m_cpConstraint, cpConstraint)
 GETTER_TEMPLATE(SPACE, c_cpSpace, cpSpace)
@@ -81,7 +79,8 @@ VALUE ARBWRAP(cpArbiter *arb);
 
 // Like vget, but returns a zero vector if value is nil, and
 // it also does not need to be dereferenced.
-static inline cpVect VGET_ZERO(VALUE value) {
+static inline cpVect
+VGET_ZERO(VALUE value) {
   if(NIL_P(value)) return cpvzero;
   return *VGET(value);
 }
@@ -106,9 +105,9 @@ void Init_cpArbiter(void);
 #define CP_BOOL_INT(VAL) (((VAL) == Qnil) || ((VAL) == Qfalse) ? 0 : 1)
 
 // transforms a C value (pointer or int) to Qtrue of Qfalse
-// For consistency with CP_BOOL_INT. 
+// For consistency with CP_BOOL_INT.
 #define CP_INT_BOOL(VAL) ((VAL) ? Qtrue : Qfalse)
- 
+
 
 
 
