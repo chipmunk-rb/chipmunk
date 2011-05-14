8f6be95961c7b1258e8e1676aacd3cf4c6a46432
Beoran <beoran@rubyforge.org>
2011-05-14 23:34:10 +0200

    Issue 7. Vendor chipmunk cleanly. Found a way to vendor chipmunk cleanly although it uses a trick in extconf.rb.

--- ext/chipmunk/extconf.rb
+++ ext/chipmunk/extconf.rb
@@ -2,31 +2,65 @@ require 'mkmf'
 
 RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
 
+if ARGV.member?('--help') || ARGV.member?('-?') 
+  puts "ruby extconf.rb:"
+  puts "Options for the Ruby bindings to Chipmunk: "
+  puts "--disable-vendor    Disables vendoring Chipmunk."
+  puts "--enable-vendor     Enables vendoring Chipmunk."
+  puts
+  exit
+end
+
+
 dir_config('chipmunk')
+
+VENDORED_CHIPMUNK     = 'chipmunk-5.3.4'
+VENDORED_SRC_DIR      =  File.join('vendor', VENDORED_CHIPMUNK, 'src')
+VENDORED_SRC_DIR2     =  File.join('vendor', VENDORED_CHIPMUNK, 'src', 'constraints')
+VENDORED_INCLUDE_DIR  =  File.join('vendor', VENDORED_CHIPMUNK, 'include', 'chipmunk')
+
 
 MINGW = '/usr/i586-mingw32msvc'
 CHIPMUNK_HEADER   = 'chipmunk.h'
 CHIPMUNK_NAME     = 'chipmunk'
 CHIPMUNK_FUNCTION = 'cpMomentForPoly'
-CHIPMUNK_INCLUDE  = ['/usr/include', 
+CHIPMUNK_INCLUDE  = [ '/usr/include', 
                       File.join(MINGW, 'include'),
                       File.join(MINGW, 'include', 'chipmunk'),
                      '/usr/local/include', 
                      '/usr/include/chipmunk',
                      '/usr/local/include/chipmunk'
                     ]
-CHIPMUNK_LIBDIR   = ['/usr/lib', 
-File.join(MINGW, 'lib'),
-'/usr/local/lib']
+CHIPMUNK_LIBDIR   = ['/usr/lib', File.join(MINGW, 'lib'), '/usr/local/lib']
 
-unless find_header(CHIPMUNK_HEADER, *CHIPMUNK_INCLUDE)
-  raise "Could not find Chipmunk headers!" 
+# This is a bit of a trick to cleanly vendor the chipmunk C library.
+sources           = Dir.glob('rb_*.c').to_a
+# normally, we need the rb_xxx files...
+
+if enable_config("vendor", true)
+  unless find_header(CHIPMUNK_HEADER, VENDORED_INCLUDE_DIR)
+    raise "Vendored chipmunk #{VENDORED_INCLUDE_DIR} not found!"
+  end
+  CHIPMUNK_INCLUDE .unshift(VENDORED_INCLUDE_DIR)
+  sources        += Dir.glob(File.join(VENDORED_SRC_DIR, '*.c'))
+  sources        += Dir.glob(File.join(VENDORED_SRC_DIR2, '*.c'))
+  # when vendoring, also include the needed c files files...
+  
+  
+else
+  unless find_header(CHIPMUNK_HEADER, *CHIPMUNK_INCLUDE)
+    raise "Could not find Chipmunk headers!" 
+  end
+
+  unless find_library(CHIPMUNK_NAME, CHIPMUNK_FUNCTION, *CHIPMUNK_LIBDIR)
+    raise "Could not link to Chipmunk library!" 
+  end
 end
 
+# this is the core of the trick, it overrides create_makefile's looking for 
+# based on the c files
+$objs = sources.map { |s| s.gsub(/.c\Z/,'.o') }
 
-unless find_library(CHIPMUNK_NAME, CHIPMUNK_FUNCTION, *CHIPMUNK_LIBDIR)
-  raise "Could not link to Chipmunk library!" 
-end
 
 =begin
 have_header('chipmunk.h', include_dirs)
@@ -37,7 +71,7 @@ if ARGV[0] == "macosx"
     $LDFLAGS += ' -arch x86_64 -arch i386 -arch ppc'
 end
 
-if ARGV[0] == "64" 
+if enable_config("64", false)
   $CFLAGS += ' -m64'
 end
  
