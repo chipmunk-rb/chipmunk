c85177b02c3cf8324d6affe8c1648696e717baca
Shawn Anderson <shawn42@gmail.com>
2013-03-12 09:24:37 -0400

    use rspec helper properly

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,13 +6,18 @@ require 'chipmunk'
 # require 'chipmunk/unsafe'
 include CP
 
-# TODO how to get this in rspec nicely?
-def check_accessor(let_name, accessor_name, value)
-  it "can set / get #{accessor_name}" do
-    object = self.send(let_name)
-    object.public_methods.should include(accessor_name)
-    object.public_methods.should include("#{accessor_name}=".to_sym)
-    object.send("#{accessor_name}=", value)
-    object.send("#{accessor_name}").should == value
+module AccessorHelpers
+  def check_accessor(let_name, accessor_name, value)
+    it "can set / get #{accessor_name}" do
+      object = self.send(let_name)
+      object.public_methods.should include(accessor_name)
+      object.public_methods.should include("#{accessor_name}=".to_sym)
+      object.send("#{accessor_name}=", value)
+      object.send("#{accessor_name}").should == value
+    end
   end
+end
+
+RSpec.configure do |c|
+  c.extend AccessorHelpers
 end
